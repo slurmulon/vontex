f1df7f0ecf1cf89ad6949f942e1ae673365c2472
evavro <me@madhax.io>
2019-05-04 17:00:49 -0700

    :art: :construction: Lots of progress towards integrating with core Vuex library and component helpers

--- tests/unit/lib/helpers.spec.js
+++ tests/unit/lib/helpers.spec.js
@@ -1,17 +1,49 @@
-import { expect } from 'chai'
+// @see: https://github.com/vuejs/vuex/blob/dev/test/unit/helpers.spec.js
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+// import ContextStore from '../../../lib/store'
+// import { mapState, mapActions, resolveNamespace } from '../../../lib/helpers'
 import { resolveNamespace } from '../../../lib/helpers'
+import { expect } from 'chai'
+
+Vue.use(Vuex)
 
 describe('helpers', () => {
+  // INPROG
+  // xdescribe('mapState', () => {
+  //   const store = new ContextStore({
+  //     namespace: 'foo',
+  //     state: {
+  //       all: [
+  //         { id: 1, name: 'a' },
+  //         { id: 2, name: 'b' }
+  //       ],
+
+  //       selected: 1
+  //     }
+  //   })
+
+  //   const vm = new Vue({
+  //     store,
+  //     context ({ mapState }) {
+  //       return {
+  //         computed: mapState('foo', ['selected'])
+  //       }
+  //     }
+  //   })
+
+  //   it('still works as a native Vuex store', () => {
+  //     console.log('VM SELECTED', vm.selected)
+  //     expect(vm.selected).to.be(1)
+  //   })
+  // })
+
   describe('resolveNamespace', () => {
     const withSelected = {
       state: {
         foo: {
-          selected: 123,
-          123: {
-            bar: {
-              selected: 456
-            }
-          }
+          selected: 123
         }
       },
 
@@ -19,6 +51,16 @@ describe('helpers', () => {
         current: () => ({
           id: 123
         })
+      },
+
+      modules: {
+        123: {
+          state: {
+            bar: {
+              selected: null
+            }
+          }
+        }
       }
     }
 
